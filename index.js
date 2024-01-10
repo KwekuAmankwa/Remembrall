1e713ec91a11fa460c4e2e4374900535f66663bb
Oak <mickyodum@gmail.com>
2024-01-10 16:22:22 +0100

    initializing and configuring firebase

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const appSettings = {
+    databaseURL: "https://groceries---scrimba-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputField = document.getElementById("input-field")
 const addBtn = document.getElementById("add-button")
 
