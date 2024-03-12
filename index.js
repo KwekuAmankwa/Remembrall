7606db3f393328cf34adef56f9388eef1ba71ab0
Oak <mickyodum@gmail.com>
2024-03-12 04:32:17 +0100

    complete update to the web app with authentication and error messages

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore, 
+        collection, 
+        addDoc, 
+        doc, 
+        deleteDoc,
+        query,
+        where,
+        onSnapshot } 
+from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js'
+import { getAuth, 
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
+        GoogleAuthProvider,
+        signInWithPopup,
+        onAuthStateChanged,
+        signOut } 
+from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js"
 
-const appSettings = {
-    databaseURL: "https://groceries---scrimba-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCK-2qFTZOgVfxdZJ6LGq_tJcGRdBiiGo0",
+  authDomain: "groceries---scrimba.firebaseapp.com",
+  databaseURL: "https://groceries---scrimba-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "groceries---scrimba",
+  storageBucket: "groceries---scrimba.appspot.com",
+  messagingSenderId: "167845050379",
+  appId: "1:167845050379:web:5548b6cf0740722a746f0e"
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const collectionName = "lists"
+const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider()
+
 
 // ---- Views ----
 const loggedInView = document.getElementById("logged-in-view")
 const loggedOutView = document.getElementById("logged-out-view")
 
 // --- Signed Out View elements ---
+const userAlertsEl = document.getElementById("user-alerts")
 const emailField = document.getElementById("email-field")
 const passwordField = document.getElementById("password-field")
 const emailSignInBtn = document.getElementById("email-sign-in")
 const createAccountBtn = document.getElementById("create-account")
+const googleSignInBtn = document.getElementById("google-sign-in")
 
 // --- Signed In View elements ----
+const itemAlertsEl = document.getElementById("item-alerts")
 const inputField = document.getElementById("input-field")
 const addBtn = document.getElementById("add-button")
 const groceryList = document.getElementById("shopping-list")
@@ -28,15 +58,14 @@ const signOutBtn = document.getElementById("sign-out-btn")
 
 // --- event-listeners ---
 
-addBtn.addEventListener("click", function(){
-    let groceries = inputField.value
-    push(shoppingListInDB, groceries)
-    
-    clearInputField()
-})
+addBtn.addEventListener("click", addButtonPressed)
 
-emailSignInBtn.addEventListener("click", showLoggedInView)
-signOutBtn.addEventListener("click", showLoggedOutView)
+createAccountBtn.addEventListener("click", authCreateAccountWithEmail)
+
+emailSignInBtn.addEventListener("click", authSignInWithEmail)
+signOutBtn.addEventListener("click", authSignOut)
+
+googleSignInBtn.addEventListener("click", authSignInWithGoogle)
 
 
 
@@ -60,51 +89,156 @@ function showLoggedInView() {
     showView(loggedInView)
 }
 
-showLoggedInView()
 
-// --- firebase console ---
-onValue(shoppingListInDB, function(snapshot){
 
-    if (snapshot.exists()){
+// --- firebase console Authentication ---
 
-        let itemsArray = Object.entries(snapshot.val())
-
-        clearGroceryList()
-
-        for (let i = 0; i < itemsArray.length; i++){
-
-            let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
-            addListItem(currentItem)
+function authStateCheck() {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            showLoggedInView()
+            renderItems()
+        } else {
+            showLoggedOutView()
         }
-    }else{
-        groceryList.innerHTML = "No Items Here... Yet."
-    }
-})
+      })
+}
+authStateCheck()
 
+
+function authCreateAccountWithEmail() {
+    const email = emailField.value
+    const password = passwordField.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+           const alert = "Account created. Please Sign In"
+            userAlertMessage(alert)
+            clearAuthFields()
+        })
+        .catch((error) => {
+            console.log(error.message)
+            const alert = "Please enter valid email and password"
+            userAlertMessage(alert)
+        })
+
+}
+
+function authSignInWithEmail() {
+    const email = emailField.value
+    const password = passwordField.value
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            clearAuthFields()
+        })
+        .catch((error) => {
+            console.log(error.message)
+            const alert = "Invalid email or password."
+            userAlertMessage(alert)
+        })
+}
+
+function authSignInWithGoogle(){
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        
+      }).catch((error) => {
+       console.log(error.message)
+      })
+}
+
+function authSignOut(){
+    signOut(auth).then(() => {
+        showLoggedOutView()
+        clearAuthFields()
+      }).catch((error) => {
+        console.log(error.message)
+      })
+}
+
+
+// --- firebase console Add and Delete Item ---
+
+async function createListItem(itemName,user){
+    if (itemName === ""){
+        const alert = "Please enter valid item"
+        itemAlertMessage(alert)
+    }else{
+        clearGroceryList()
+        const docRef = await addDoc(collection(db, collectionName), {
+            item: itemName,
+            uid: user.uid
+        })
+        renderItems()
+    }
+}
+
+async function deleteListItem(itemId) {
+    await deleteDoc(doc(db, collectionName, itemId))
+    clearGroceryList()
+    renderItems()
+}
+
+async function renderItems() {
+    const user = auth.currentUser
+    const q = query(collection(db, collectionName), where("uid", "==", user.uid))
+    onSnapshot(q, (querySnapshot) => {
+        clearGroceryList()
+        querySnapshot.forEach((doc) => {
+            createListContent(doc)
+        })
+    })    
+}
+
+
+
+// --- UI functions ---
+
+function userAlertMessage(alert){
+    userAlertsEl.textContent = alert
+    return userAlertsEl
+}
+
+function itemAlertMessage(alert) {
+    itemAlertsEl.textContent = alert
+    return userAlertsEl
+}
+
+function addButtonPressed(){
+    const item = inputField.value
+    console.log(item)
+    const user = auth.currentUser
+    createListItem(item,user)
+    clearInputField()
+}
+
+function createListContent(itemData){
+    const itemId = itemData.id
+    const itemName = itemData.data()
+    const listItem = document.createElement("li")
+    listItem.innerHTML = itemName.item
+    listItem.addEventListener("dblclick", function (){
+        deleteListItem(itemId)
+    })
+    groceryList.appendChild(listItem)
+}
+
+
+// --- clear input fields ---
 function clearInputField(){
     inputField.value = ""
 }
 
-function addListItem(item){
-    // groceryList.innerHTML += `<li> ${item}</li>`
-    let itemID = item[0]
-    let itemValue = item[1]
-
-    let newElement = document.createElement("li")
-    newElement.textContent = itemValue
-
-    newElement.addEventListener("dblclick", function(){
-        let itemLocationInDB = ref(database, `shoppingList/${itemID}`)
-
-        remove(itemLocationInDB)
-    })
-
-    groceryList.append(newElement)
-}
-
 function clearGroceryList(){
     groceryList.innerHTML = ""
+}
+
+function clearField(field){
+    field.value = ""
+}
+
+function clearAuthFields(){
+    clearField(emailField)
+    clearField(passwordField)
 }
